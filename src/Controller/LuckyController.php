8f086c0585698e8bbea0fd305a27767c2e042828
Nestaan <nesta.contact@gmail.com>
2021-02-08 23:52:33 +0100

    Première page Symfony

---
+++ src/Controller/LuckyController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LuckyController extends AbstractController
+{
+    /**
+     * @Route("/lucky/number", name="app_lucky_number")
+     */
+    public function number()
+    {
+        $number = mt_rand(0, 100);
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+}
